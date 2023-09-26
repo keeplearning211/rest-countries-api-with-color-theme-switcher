21f399fc5eab216aa717bd148850470f59fb674e
Dong <dong211cloudinarytraining@gmail.com>
2023-09-26 21:36:03 +0700

    create country card

---
+++ src/components/CountryCard.tsx
@@ -0,0 +1,77 @@
+// components/CountryCard.tsx
+import { Box, Card, CardContent, Typography, useTheme } from '@mui/material';
+
+interface Country {
+  name: string;
+  population: number;
+  region: string;
+  capital: string;
+  flag: string;
+}
+
+interface CountryCardProps {
+  country: Country;
+}
+
+function CountryCard({ country }: CountryCardProps) {
+  const theme = useTheme()
+
+  const clickHandler = () => {
+    console.log('countryName', country.name);
+  }
+
+  return (
+    <Card
+      sx={{
+        width: {
+          sx: '100%',
+          md: theme.spacing(33)
+        },
+        height: {
+          sx: 'auto',
+          md: theme.spacing(46.75)
+        },
+        padding: theme.spacing(0),
+        backgroundColor: 'background.default',
+        borderRadius: theme.spacing(1),
+      }}
+      onClick={clickHandler}
+    >
+      <CardContent
+        sx={{
+          padding: 0,
+          cursor: 'pointer',
+        }}>
+        <img src={country.flag} alt={country.name} style={{ width: '100%', height: 158 }} />
+        <Box
+          sx={{
+            padding: theme.spacing(3, 2.5),
+            display: 'grid',
+            gap: theme.spacing(3)
+          }}>
+          <Typography variant="h5" component="div">
+            {country.name}
+          </Typography>
+          <Box sx={{
+            display: 'grid',
+            gap: theme.spacing(1),
+          }}>
+
+            <Typography variant="body2" color="text.secondary">
+              Population: {country.population}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Region: {country.region}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Capital: {country.capital}
+            </Typography>
+          </Box>
+
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default CountryCard;
