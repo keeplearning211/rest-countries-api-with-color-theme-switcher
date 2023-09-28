62770bb1b7eb7c56f58c2f8f70dce8dafd4ae9b1
Dong <dong211cloudinarytraining@gmail.com>
2023-09-28 20:11:27 +0700

    Update icons to match design.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,9 @@
 import { Box, IconButton, useTheme, Typography } from '@mui/material'
 import React from 'react'
 import ThemeContext from '../theme/themeContext'
-import Brightness4Icon from '@mui/icons-material/Brightness4'
-import Brightness7Icon from '@mui/icons-material/Brightness7'
-
-
-
+// import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 function Header() {
   const theme = useTheme();
   const { toggleMode } = React.useContext(ThemeContext);
@@ -16,15 +14,15 @@ function Header() {
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'space-between',
-          bgcolor: 'background.default',
-          color: 'text.primary',
+          // bgcolor: 'background.default',
+          // color: 'text.primary',
           padding: '1.25rem 1rem'
         }}
       >
         <Typography variant='h1'>Where in the world?</Typography>
         <Box>
           <IconButton sx={{ ml: 1 }} onClick={toggleMode} color="inherit">
-            {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+            {theme.palette.mode === 'dark' ? <DarkModeOutlinedIcon /> : <DarkModeIcon />}
           </IconButton>
           <Typography component="span" variant='body2' sx={{ textTransform: 'capitalize', fontWeight: 600 }}>
             {theme.palette.mode} mode
